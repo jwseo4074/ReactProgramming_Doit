38ab867f8c33f4bf95dc10013ed13bceeebae646
zwon2056 <zwon2056@gmail.com>
2022-05-29 18:12:19 +0900

    Add AxiosExamplePage using axios

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 // import {  Link, Route, Routes, Router} from 'react-router-dom';
 // 이건 안돼
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 //  이건 돼
 
 import Header from './Pages/Header';
@@ -14,54 +14,50 @@ import Example3Page from './Pages/Example3Page';
 import Example4Page from './Pages/Example4Page';
 import Example5Page from './Pages/Example5Page';
 import MaterialUI01 from './Main/Material_UI/materialUI01';
+import AxiosExamplePage from './Pages/AxiosExamplePage';
 
 function App() {
+	return (
+		//
 
-  return (  
-    //  
+		//     {/* <TopHeader/> */}
+		//     <Header/>
+		//     <hr></hr>
+		//     {/* <Route exact path = "/" component={} />
+		//     <Route path="/about" component={About} />
+		//     <Route path*/}
 
-    //     {/* <TopHeader/> */}
-    //     <Header/>
-    //     <hr></hr>
-    //     {/* <Route exact path = "/" component={} />
-    //     <Route path="/about" component={About} />
-    //     <Route path*/}
-        
-    //     {/* <Main/>  */}
+		//     {/* <Main/>  */}
 
-    //     <div>
-    //       <Route exact path="/" component={Home}/>
-    //       <Route path="/about" component={About}/>
+		//     <div>
+		//       <Route exact path="/" component={Home}/>
+		//       <Route path="/about" component={About}/>
 
-    //       {/* <Route path="/03page" component={Example3Page}/> */}
-    //     </div>
-    // </Router>
+		//       {/* <Route path="/03page" component={Example3Page}/> */}
+		//     </div>
+		// </Router>
 
+		<Router>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/03page" element={<Example3Page />} />
+				<Route path="/04page" element={<Example4Page />} />
+				<Route path="/05page" element={<Example5Page />} />
+				<Route path="/axiosexamplepage" element={<AxiosExamplePage />} />
+				<Route path="/material" element={<MaterialUI01 />} />
+			</Routes>
+		</Router>
 
-      <Router> 
-        <Header/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route exact path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/03page" element={<Example3Page/>}/>
-          <Route path="/04page" element={<Example4Page/>}/>
-          <Route path="/05page" element={<Example5Page/>}/>
-          <Route path="/material" element={<MaterialUI01/>}/>
-        </Routes>
-      </Router> 
-
-          
-
-          // {/* <Route exact path="/" component={Home}/> */}
-          // {/* <Switch>
-          //     <Route path="/about/:name" component={About}/>
-          //     <Route path="/about" component={About}/>
-          // </Switch> */}
-          // {/* <Route path="/about/:name" component={About}/>
-          // <Route path="/about" component={About}/> */}
-
-  );
+		// {/* <Route exact path="/" component={Home}/> */}
+		// {/* <Switch>
+		//     <Route path="/about/:name" component={About}/>
+		//     <Route path="/about" component={About}/>
+		// </Switch> */}
+		// {/* <Route path="/about/:name" component={About}/>
+		// <Route path="/about" component={About}/> */}
+	);
 }
 
 export default App;
--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -1,38 +1,46 @@
 import React from 'react';
-import {  Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
+function Header() {
+	return (
+		<>
+			<Link to="/">
+				<button>Home</button>
+			</Link>
 
-function Header () {
-    return (
-        <>
-            <Link to="/">
-            <button>Home</button>
-            </Link>   
-                
-            <Link to="/03page">
-            <button>03 page</button>
-            </Link>
-            <Link to="/04page">
-            <button>04 page</button>
-            </Link>
-            <Link to="/05page">
-            <button>05 page</button>
-            </Link>
+			<Link to="/03page">
+				<button>03 page</button>
+			</Link>
+			<Link to="/04page">
+				<button>04 page</button>
+			</Link>
+			<Link to="/05page">
+				<button>05 page</button>
+			</Link>
 
-            <Link to="/material">
-            <button> Material page</button>
-            </Link>
-            
-            <hr></hr>
-            
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/03page">03 Page</Link></li>
-            </ul>
-            <hr/>
-        </>
-    )
+			<Link to="/material">
+				<button> Material page</button>
+			</Link>
+
+			<Link to="/axiosexamplepage">
+				<button> AxiosExamplePage</button>
+			</Link>
+			<hr></hr>
+
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/about">About</Link>
+				</li>
+				<li>
+					<Link to="/03page">03 Page</Link>
+				</li>
+			</ul>
+			<hr />
+		</>
+	);
 }
-        
+
 export default Header;
